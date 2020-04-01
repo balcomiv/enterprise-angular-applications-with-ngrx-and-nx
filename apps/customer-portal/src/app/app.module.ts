5120ff4d6272e29b160ee73079ccff453029b72d
balcomiv <35431931+balcomiv@users.noreply.github.com>
2020-04-01 06:21:24 -0400

    Add - Lib - Auth

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import {
+  AuthModule,
+  authRoutes
+} from '@enterprise-angular-applications-with-ngrx-and-nx/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([{ path: 'auth', children: authRoutes }], {
+      initialNavigation: 'enabled'
+    }),
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]

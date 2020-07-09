038815ee3e9f5850c8ff63324f57d1b5edc42bfa
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-07-09 09:50:56 -0400

    update - layout component

--- libs/auth/src/lib/services/auth.service.ts
+++ libs/auth/src/lib/services/auth.service.ts
@@ -2,14 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthenticationModel, User } from '@enterprise-angular/data-models';
 import { EventEmitter } from 'events';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private userSubeject$ = new BehaviorSubject<User>(null);
+  private userSubeject$ = new ReplaySubject<User>(1);
   user$ = this.userSubeject$.asObservable();
 
   private unsubscribe = new EventEmitter();
